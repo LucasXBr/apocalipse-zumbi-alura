7c6a958c7afad819032a92315f771706d203fe61
LucasXbr <lucasxbr15@gmail.com>
2023-08-14 19:01:06 -0300

    first commit

---
+++ Assets/ControlaJogador.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaJogador : MonoBehaviour
+{
+
+    public float Velocidade = 10;
+    Vector3 direcao;
+
+    void Update()
+    {
+        //Inputs do Jogador - Guardando teclas apertadas
+        float eixoX = Input.GetAxis("Horizontal");
+        float eixoZ = Input.GetAxis("Vertical");
+
+        direcao = new Vector3(eixoX, 0, eixoZ);
+
+        //Animações do Jogador
+        if (direcao != Vector3.zero)
+        {
+            GetComponent<Animator>().SetBool("Movendo", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("Movendo", false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //Movimentação do Jogador por segundo
+        GetComponent<Rigidbody>().MovePosition
+            (GetComponent<Rigidbody>().position +
+            (direcao * Velocidade * Time.deltaTime));
+    }
+}
