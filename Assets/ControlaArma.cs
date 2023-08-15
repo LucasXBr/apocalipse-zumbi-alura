94144008b8417442987c87f2dfb924b903d302b0
LucasXbr <lucasxbr15@gmail.com>
2023-08-15 18:34:54 -0300

    personagem tem animação e os inimigos podem te atacar mas tem algums erros para serem corrigidos ainda

---
+++ Assets/ControlaArma.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaArma : MonoBehaviour 
+{
+
+    public GameObject Bala;
+    public GameObject CanoDaArma;
+
+    // Use this for inicialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(Bala, CanoDaArma.transform.position, CanoDaArma.transform.rotation);
+        }
+    }
+}
